8a3128fa66197c36d6791a36ab70431d5c0dbd19
Riya bansal <riya28bansal@gmail.com>
2022-09-14 05:03:52 +0530

    SignupSection completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,9 @@ const express = require("express")
 const { body, validationResult } = require('express-validator');
 const User = require ("../models/User")
 const router = express.Router();
+const bcrypt = require("bcryptjs")
+const JWT_SECRET = 'riyaisastudent'
+const jwt = require('jsonwebtoken');
 
 
 //create a user using: POST "/api/auth/createUser". no login required
@@ -19,12 +22,24 @@ router.post("/createUser",[
     if(user){
         return res.status(400).json({error: "sorry a user with this address already exists"})
     }
+    const salt = await bcrypt.genSaltSync(10);
+    const secPass= await bcrypt.hashSync(req.body.password, salt)
+        //create a user
         user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       })
-      res.json(req.body)
+
+      const data ={
+        user:{
+            id : user.id
+        }
+      }
+      const authToken = jwt.sign(data,JWT_SECRET)
+
+      //res.json(user)
+      res.json({authToken})
     } catch (error){
         console.log(error.message)
         res.status(500).send("some error occured")
